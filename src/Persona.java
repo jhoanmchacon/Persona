301ac79446bdc3ba63311d7b4625b4467d265eb8
Maria <cariasseguramaria@gmail.com>
2015-10-23 15:56:34 -0430

    otros cambios en persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,12 +1,11 @@
 
-//Programa Persona
-//import java.util.Arrays;// libreria para vectores mas comentarios jhoann
-import java.util.Scanner;// mas comentarios jhoanimport java.util.Scanner;
 
-//librerias de la clase
+import java.util.Scanner;
+
+
 	public class Persona {// otro cambio
-		private static String nombre;//jhoan chacongggggg
-		private static String apellido;//declaracion de variable apellido
+		private static String nombre;//jhoan chacon
+		private static String apellido;//declaracion de variable
 		private static int edad;
 		private static String cedula;
 		private static String telefono;
@@ -51,64 +50,16 @@ import java.util.Scanner;// mas comentarios jhoanimport java.util.Scanner;
 					System.out.println("\n datos incorrectos");
 				}
 			}
-<<<<<<< HEAD
-=======
-public class Persona {// otro cambio
-    private static String nombre;//jhoan chacongggggg
-	private static String apellido;//declaracion de variable apellido
-	private static int edad;
-	private static String cedula;
-	private static String telefono;
-	private static String val;
-	private static String pais;
-	 
-	static Scanner in = new Scanner (System.in);
-
-	
-	public void introducirDatos(){//pruebas
-		System.out.println("Introduce tu nombre:");
-		nombre = in.next();
-		System.out.println("Introduce tu apellido:");
-		apellido = in.next();
-		System.out.println("Introduce tu edad:");
-		edad = in.nextInt();
-		System.out.println("Introduce tu cedula:");
-		cedula = in.next();
-		System.out.println("Introduce tu numero de telefono:");
-		telefono = in.next();
-		System.out.println("Introduce el país donde naciste:");
-		pais= in.next();
-						
-		
-	}
-
-	public void mayorEdad(){
-		if (edad>=18){
-			System.out.println("\n Es mayor de edad");
-		}
-		else {
-			System.out.println("\n Es menor de edad");
-		}
-	}
-	public void comprobarNombre(){
-		
-		if (nombre.equals("maria")){
-			System.out.println("\n datos correctos");	
-		}
-		else{
-			System.out.println("\n datos incorrectos");
-		}
-	}
-	
-	public void Imprimir(){
-	System.out.println("\n Sr(a) Por favor verifique si sus datos son Correctos");
-	System.out.println("\n Nombre:"+ nombre +(" ")+ apellido);
-	System.out.println("\n Edad:" + edad);
-	System.out.println("\n Cedula:" + cedula);
-	System.out.println("\n Telefono:" + telefono);
-	System.out.println("\n Pais de nacimiento:" + pais);
-=======
->>>>>>> 799221898ff4291a543f31268c054c3b6146cd9b
+			
+			public void verNacionalidad(){
+				if (pais.equals("venezuela")){
+					System.out.println("Es venezolano");
+				}
+				else{
+					System.out.println("Es extranjero");
+				}
+				
+			}
 	
 	public void verificacionInf(){
 		System.out.print("\n\n Sr(a) "+ nombre + " sus datos son Correctos Y/N: ");
@@ -123,26 +74,8 @@ public class Persona {// otro cambio
 			verificacionInf();
 			}
 	}
-	public void verNacionalidad(){
-		if (pais.equals("venezuela")){
-			System.out.println("Es venezolano");
-		}
-		else{
-			System.out.println("Es extranjero");
-		}
 
-<<<<<<< HEAD
-	}
-			
-	/*System.out.println("\n Vector de datos");
-	Object[] p1= {nombre, apellido, edad, cedula, telefono, pais};
-	for (Object value: p1);
-	System.out.println(Arrays.deepToString(p1));*/
-
- } 
-=======
-	}	
-}
 	
->>>>>>> 799221898ff4291a543f31268c054c3b6146cd9b
+}
+
 
